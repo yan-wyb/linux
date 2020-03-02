f120bfdb25419bf7f3f03cc8b62f905fa545d0d0
Xihai Zhu <xihai.zhu@amlogic.com>
2020-03-02 18:00:49 +0800

    amvecm: do not override lc_en [1/1]
    PD#SWPL-21418
    
    Problem:
    lc enabled again under pc mode
    after signal change
    
    Solution:
    db setting should not override
    enable flag
    
    Verify:
    tl1
    
    Change-Id: I4cacb7cc260bcf41de3cae4c5c82425d98949b76
    Signed-off-by: Xihai Zhu <xihai.zhu@amlogic.com>

--- drivers/amlogic/media/enhancement/amvecm/amcm.c
+++ drivers/amlogic/media/enhancement/amvecm/amcm.c
@@ -270,22 +270,11 @@ void am_set_regmap(struct am_regs_s *p)
 						SHARP1_DEMO_CRTL))
 						break;
 				}
-			/*if the bit 4 of SRSHARP1_LC_TOP_CTRL is 1,
-			 *it means that lc is enable in db,
-			 *so need to change lc_en to 1;
-			 *else if the bit 4 of SRSHARP1_LC_TOP_CTRL is 0,
-			 *it means that lc is disable in db,
-			 *so need to change lc_en to 0
-			 */
+
 				if (p->am_reg[i].addr == SRSHARP1_LC_TOP_CTRL) {
-					temp =
-					(p->am_reg[i].val & p->am_reg[i].mask)
-						>> 4;
-					temp &= 0x1;
-					if (!temp && lc_en)
-						lc_en = 0;
-					else if (!lc_en && temp)
-						lc_en = 1;
+					if (!lc_en)
+						p->am_reg[i].val =
+						p->am_reg[i].val & 0xffffffef;
 				}
 				if (pq_reg_wr_rdma)
 					VSYNC_WR_MPEG_REG(p->am_reg[i].addr,
