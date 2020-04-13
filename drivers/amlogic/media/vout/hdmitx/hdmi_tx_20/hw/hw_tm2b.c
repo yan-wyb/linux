eeaa299da8ec36318d50219ba49fec773fd9b512
Zongdong Jiao <zongdong.jiao@amlogic.com>
2020-04-13 15:39:43 +0800

    hdmitx: fine tune 7-2 VL value under 480p [2/2]
    PD#SWPL-24086
    
    Problem:
    the 7-2 VL value is cirtical
    
    Solution:
    fine tune 7-2 VL value under 480p
    
    Verify:
    tm2b
    
    Change-Id: I251cf9e3e24e51c3c12a4a6314a203b5e940f8d2
    Signed-off-by: Zongdong Jiao <zongdong.jiao@amlogic.com>

--- drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hw_tm2b.c
+++ drivers/amlogic/media/vout/hdmitx/hdmi_tx_20/hw/hw_tm2b.c
@@ -31,14 +31,14 @@ void set_tm2b_phy_para(unsigned int mode)
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x0000080b);
 		break;
 	case HDMI_PHYPARA_3G: /* 2.97Gbps */
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x31cb6262);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb62a3);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL3, 0x2ab0ff3b);
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000002);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000003);
 		break;
-	case HDMI_PHYPARA_270M: /* 1.485Gbps, and below */
-	case HDMI_PHYPARA_DEF:
+	case HDMI_PHYPARA_270M: /* 270Mbps */
+	case HDMI_PHYPARA_DEF: /* 1.485Gbps, and others */
 	default:
-		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb6262);
+		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL0, 0x33eb5252);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL3, 0x2ab0ff3b);
 		hd_write_reg(P_TM2_HHI_HDMI_PHY_CNTL5, 0x00000003);
 		break;
